3f07fae671b07dd4158aa30b6e33b240562e8123
consolatahchithyoka-max <consolatahchithyoka@gmail.com>
2025-11-27 22:07:23 +0200

    Update MovieCard.jsx

--- my-react-app/src/components/MovieCard.jsx
+++ my-react-app/src/components/MovieCard.jsx
@@ -1,11 +1,18 @@
 import react from "react";
 import { Link } from "react-router-dom";
+import { moviesData } from "../data/moviesData";
+import MCard from "./Card";
+import { Grid } from '@mui/material';
 
 export const MovieCard = () => {
+    const moviesdata = moviesData.results
     return(
         <>
             <h1>Movie list in movie cards</h1>
             <Link to="/">Go Back</Link>
+            <Grid container spacing={4} sx={{ p: 4,  }} justifyContent="center">
+                {moviesdata.map(moviedata => (<MCard title={moviedata.title} description={moviedata.overview} image={moviedata.poster_path} rating={moviedata.vote_average} genre={moviedata.genre_ids} year={moviedata.release_date}  />))}
+            </Grid>
         </>
     )
 }
